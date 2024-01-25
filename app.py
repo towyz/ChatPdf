501bb97d91c29cb3917a3ca6385aab61c560dbb0
unknown <towyz12138@gmail.com>
2024-01-25 22:32:18 +0800

    flask -> from pdf to certain chat

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, jsonify, redirect, render_template, url_for
 from Controller.PdfController import pdfController
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "SESSION_NEED_THIS_12138"
 app.register_blueprint(pdfController)
+
+
+@app.route("/get_conversation_dir")
+def get_conversation_dir():
+    """Get the dir of pdfs."""
+    with open("./settings/pdf_source.json", "r") as file:
+        conversation_data = json.load(file)
+    return jsonify(conversation_data)
+
+
+@app.route("/get_conversation_data/<conversation_id>")
+def get_conversation_data(conversation_id):
+    with open("./history/" + conversation_id + ".json", "r") as file:
+        conversation_data = json.load(file)
+    return jsonify(conversation_data)
+
+
+@app.route("/chat/<conversation_id>")
+def chat(conversation_id):
+    return render_template("history-check04.html", conversation_id=conversation_id)
 
 
 @app.route("/")
 def index():
-    return render_template("./pages/history-check04.html")
+    return render_template("show-pdf-dir.html")
 
 
 if __name__ == "__main__":
