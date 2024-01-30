8a93adbd5c9cb5f00d7ff7324928ad56c186c295
unknown <towyz12138@gmail.com>
2024-01-30 14:10:49 +0800

    Added the function to add files, updated the README.md.

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ import time
 
 import requests
 from flask import Flask, jsonify, render_template, request
-from Controller.PdfController import pdfController
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SESSION_NEED_THIS_12138"
-app.register_blueprint(pdfController)
+app.config["UPLOAD_FOLDER"] = "./paper"
 
 
 @app.route("/get_conversation_dir")
@@ -33,7 +32,7 @@ def chat(conversation_id):
 
 @app.route("/")
 def index():
-    return render_template("show-pdf-dir.html")
+    return render_template("show-pdf-dir02.html")
 
 
 @app.route("/send_message/<conversation_id>", methods=["POST"])
@@ -98,6 +97,73 @@ def send_message(conversation_id):
         new_data = json.load(f)
 
     return jsonify(new_data)
+
+
+def record_pdf_source(source_id):
+    """Record the pdf source id in the json file."""
+    output_dir = "./settings"
+    listdir = os.listdir(output_dir)
+
+    if "pdf_source.json" in listdir:
+        fr = open(os.path.join(output_dir, "pdf_source.json"))  # a represents append
+        model = json.load(fr)
+        fr.close()
+
+        for i in source_id:
+            model[i] = source_id[i]
+        jsObj = json.dumps(model)
+
+        with open(
+            os.path.join(output_dir, "pdf_source.json"), "w"
+        ) as fw:
+            fw.write(jsObj)
+
+
+@app.route("/upload_file", methods=["POST"])
+def upload_file():
+    # try:
+    uploaded_file = request.files["file"]
+
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], uploaded_file.filename)
+    uploaded_file.save(file_path)
+
+    # print("File saved to: ", os.path.abspath(file_path))
+
+    # 数据准备
+    with open(
+        os.path.join(os.path.abspath("."), "settings", "API_KEYS.json")
+    ) as f:
+        api_key = json.load(f)["ChatPDF_API"]
+
+    headers = {"x-api-key": api_key}
+    files = [("file", ("file", open(file_path, "rb"), "application/octet-stream"))]
+
+    response = requests.post(
+        "https://api.chatpdf.com/v1/sources/add-file", headers=headers, files=files
+    )
+
+    if response.status_code == 200:
+        print("Source ID:", response.json()["sourceId"])
+        # store the source id in the json file
+        source_id = {response.json()["sourceId"]: file_path}
+        record_pdf_source(source_id)
+
+        # create a history file to store the chatting history
+        output_dir = "./history"
+        listdir = os.listdir(output_dir)
+        if str(response.json()["sourceId"]) + ".json" not in listdir:
+            with open(
+                os.path.join(output_dir, str(response.json()["sourceId"]) + ".json"),
+                "w",
+            ) as f:
+                f.write("{}")
+    else:
+        print("Status:", response.status_code)
+        print("Error:", response.text)
+
+    with open("./settings/pdf_source.json", "r") as file:
+        conversation_data = json.load(file)
+    return jsonify(conversation_data)
 
 
 if __name__ == "__main__":
